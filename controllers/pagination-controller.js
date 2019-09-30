2da9f0220ebdc915784018a0774607d4c51a35d7
tariq <tariqabdelghani@gmail.com>
2019-09-30 11:08:06 +0200

    added try catch block in pagination controller

--- controllers/pagination-controller.js
+++ controllers/pagination-controller.js
@@ -7,33 +7,37 @@ const ResponseObject = require('../models/response_object');
 
 function paginate(Model_) {
     return async (req, res, next) => {
-        console.log(req.params.page, req.params.perPage)
-        let skip = parseInt(req.params.page) - 1;
-        let limit = parseInt(req.params.perPage);
-        let docCount;
-        let nextState = false;
-        let previousState = true;
-        let numberOfPages;
+        try {
+            //console.log(req.params.page, req.params.perPage)
+            let skip = parseInt(req.params.page) - 1;
+            let limit = parseInt(req.params.perPage);
+            let docCount;
+            let nextState = false;
+            let previousState = true;
+            let numberOfPages;
 
-        docCount = await Model_.estimatedDocumentCount();
-        console.log(docCount);
-        let doc = await Model_.find().skip(skip).limit(limit);
-        if (skip == 0)
-            previousState = false;
-        if (docCount > parseInt(req.params.page) * limit)
-            nextState = true;
-        numberOfPages = Math.ceil(docCount / limit);
-        const resObj = new ResponseObject(
-            req.user,
-            {
-                docCount: docCount,
-                numberOfPages: numberOfPages,
-                nextState: nextState,
-                previousState: previousState,
-                doc: doc
-            }
-        )
-        res.send(resObj);
+            docCount = await Model_.estimatedDocumentCount();
+            console.log(docCount);
+            let doc = await Model_.find().skip(skip).limit(limit);
+            if (skip == 0)
+                previousState = false;
+            if (docCount > parseInt(req.params.page) * limit)
+                nextState = true;
+            numberOfPages = Math.ceil(docCount / limit);
+            const resObj = new ResponseObject(
+                req.user,
+                {
+                    docCount: docCount,
+                    numberOfPages: numberOfPages,
+                    nextState: nextState,
+                    previousState: previousState,
+                    doc: doc
+                }
+            )
+            res.send(resObj);
+        } catch(err){
+            next(err);
+        }
 
     }
 
